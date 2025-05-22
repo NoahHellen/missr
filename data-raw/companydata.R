11da14491c829dbce60b8e333cf1d1e26e019865
Noah Hellen <noahhellen@gmail.com>
2025-05-22 12:14:30 +0100

    Added back MNAR tests, now checking GHA

--- data-raw/companydata.R
+++ data-raw/companydata.R
@@ -1,18 +1,18 @@
-set.seed(12)
+set.seed(19)
 
 companydata <- data.frame(
-  sales = rexp(n = 200, rate = 0.1),
-  marketing_spend = rexp(n = 200, rate = 0.2),
-  product_rating = runif(n = 200, min = 0, max = 10),
-  employees = sample(5:100, 200, replace = TRUE),
-  gross_profit = runif(200, min = 3.5, max = 6)
+  sales = rexp(n = 500, rate = 0.1),
+  marketing_spend = rexp(n = 500, rate = 0.2),
+  product_rating = runif(n = 500, min = 0, max = 10),
+  employees = sample(5:500, 500, replace = TRUE),
+  gross_profit = runif(500, min = 3.5, max = 6)
 )
 
-mar_prob <- 0.4 * exp(-0.005 * companydata$gross_profit)
-mcar_prob <- 0.05
+mar_prob <- 0.3 * exp(-0.05 * companydata$gross_profit)
+mcar_prob <- 0.1
 
 set_na <- runif(nrow(companydata)) < mar_prob
 companydata$employees[set_na] <- NA
-companydata$product_rating[sample(1:200, size = floor(200 * mcar_prob))] <- NA
+companydata$product_rating[sample(1:500, size = floor(500 * mcar_prob))] <- NA
 
 usethis::use_data(companydata, overwrite = TRUE)
--- tests/testthat/test-mnar.R
+++ tests/testthat/test-mnar.R
@@ -14,3 +14,26 @@ test_that("mnar() warns users of non-numeric encoding", {
   expect_warning(mcar(animalhealth), "Non-numeric columns encoded - verify
     interpretable.")
 })
+
+test_that("mnar() calculates correct MCAR statistics", {
+  output <- mnar(companydata)
+  expect_equal(output$mcar$statistic, 22.6702661)
+  expect_equal(output$mcar$degrees_freedom, 11)
+  expect_equal(output$mcar$p_val, 0.019665507)
+  expect_equal(output$mcar$missing_patterns, 4)
+})
+
+test_that("mnar() calculates correct MAR statistics", {
+  output <- mnar(companydata)
+  expect_equal(output$mar$missing, c("product_rating", "employees"))
+  expect_equal(output$mar$p_value, c(0.106108131, 0.012842060))
+  expect_equal(output$mar$explanatory, c("product_rating", "gross_profit"))
+  expect_equal(
+    output$mar$p_values$product_rating,
+    c(0.611053024, 0.928502026, 0.106108131, 0.758187805, 0.260427274)
+  )
+  expect_equal(
+    output$mar$p_values$employees,
+    c(0.891781504, 0.014935005, 0.795686530, 0.862482203, 0.012842060)
+  )
+})
